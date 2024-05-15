17a92fdba8c8f81afc3a8af5533a92a185f1d415
andre <andrinho@gmail.com>
2024-05-15 19:46:12 -0300

    primeiro

---
+++ server.js
@@ -0,0 +1,33 @@
+// import http from "http";
+import app from "./src/app.js";
+
+const PORT1 = 3000;
+const PORT2 = 3002;
+
+// const rotas1 = {
+//     "/": "Curso de node.js!",
+//     "/livros": "livros de node.js",
+//     "/autores":"autores de livros de node.js"
+// }
+
+// const rotas2 = {
+//     "/": "Curso de python!",
+//     "/livros": "livros de python",
+//     "/autores":"autores de livros de python"
+// }
+
+// const server1 = http.createServer((req, res)=>{
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end(rotas1[req.url]);
+// });
+// const server2 = http.createServer((req, res)=>{
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end(rotas2[req.url]);
+// });
+
+app.listen(PORT1,()=>{
+    console.log("servidor escutando na porta 3000!")
+})
+// server2.listen(PORT2,()=>{
+//     console.log("servidor escutando na porta 3002!")
+// })
---
+++ src/config/dbConnect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+async function conectaNaDatabase(){
+    mongoose.connect("mongodb+srv://andrinho:M3W2RdPexyjlKxPa@cluster0.aqdh46g.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0")
+    return mongoose.connection;
+}
+
+export default conectaNaDatabase;
+
+//string de conexao
+//
