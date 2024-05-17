e8c27232cf813fc6c8e7aa94b56ceff24f607b34
andre <andrinho@gmail.com>
2024-05-17 04:51:40 -0300

    incluindo um ci para docker images

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase(){
-    // mongoose.connect("mongodb://root:example@mongo:27017")
     mongoose.connect(process.env.DB_CONNECTION_STRING)
     return mongoose.connection;
 }
